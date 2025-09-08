29b0105e52bd67a7159f3d5592018a4d6d3a7740
LaeliSvaad <ls@laelisvaad.com>
2025-09-08 16:04:50 +0200

    car details displayed on car page

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Car;
+use App\Repository\CarRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CarController extends AbstractController
 {
-    #[Route('/car', name: 'app_car')]
-    public function index(): Response
+    #[Route('/car/{id}', name: 'app_car')]
+    public function index(int $id, CarRepository $repository): Response
     {
+        $car = $repository->find($id);
+
         return $this->render('car/index.html.twig', [
             'controller_name' => 'CarController',
+            'car' => $car,
         ]);
     }
 }
